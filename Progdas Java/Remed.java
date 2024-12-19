97b967e769b0d1c382e8bb0dfa9162a1c2ad4a6c
Afgan Irwansyah Hidayat <96742317+Afganirwansyahhidayat@users.noreply.github.com>
2024-12-19 12:10:55 +0700

    Add files via upload

---
+++ Progdas Java/Remed.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Remed {
+    public static void main(String[] args) {
+        // Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Masukan Angka A: ");
+        int a = scanner.nextInt();
+
+        System.out.println("Masukan Angka B: ");
+        int b = scanner.nextInt();
+
+        // Mencari nilai terkecil
+        int min = (a < b) ? a : b;
+
+        // Mencari nilai terbesar
+        int max = (a > b) ? a : b;
+
+        // Menghitung rata-rata
+        double average = (a + b) / 2.0;
+
+        // Output
+        System.out.println("Nilai terkecil: " + min);
+        System.out.println("Nilai terbesar: " + max);
+        System.out.println("Rata-rata: " + average);
+        scanner.close();
+    }
+}
