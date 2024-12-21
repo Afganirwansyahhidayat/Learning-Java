133f41a4090f82644e683bb2820f91b61af3ffa5
Afgan Irwansyah Hidayat <96742317+Afganirwansyahhidayat@users.noreply.github.com>
2024-12-21 21:26:55 +0700

    Update Loop.java

--- Progdas Java/Loop.java
+++ Progdas Java/Loop.java
@@ -1,1 +1,9 @@
-
+public class Loop {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <= 30) {
+            System.out.println("Perulangan ke: " + a);
+            a++;
+        }
+    }
+}
