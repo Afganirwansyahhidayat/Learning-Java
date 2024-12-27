add0b07615de0601a178d9015c924cdb63f18756
Afgan Irwansyah Hidayat <96742317+Afganirwansyahhidayat@users.noreply.github.com>
2024-12-27 18:46:00 +0700

    Update Age1.java

--- Progdas Java/Age1.java
+++ Progdas Java/Age1.java
@@ -1,1 +1,12 @@
+public class Age1 {
+    public static void main(String[] args) {
+        int a = 18; // age
 
+        if (a >= 17) {
+            System.out.println("Anda Orang Dewasa!");
+        }
+        else {
+            System.out.println("Anda Remaja!");
+    }
+}
+}
